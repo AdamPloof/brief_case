2109fc0dfeaa43a394e6987c09223e0392d2007d
Adam Ploof <adamploof@hotmail.com>
2020-12-02 23:56:44 -0500

    initial commit

---
+++ src/Controller/CaseController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class CaseController extends AbstractController
+{
+    /**
+    * @Route("/")
+    */
+    public function index()
+    {
+        return $this->render("cases/index.html.twig");
+    }
+}

174573da1b4f6c5b02b8d1f0025dbfea3fadb76f
Adam Ploof <adamploof@hotmail.com>
2020-12-13 20:10:17 -0500

    updated CaseFile document mapping

--- src/Document/Case.php
+++ src/Document/Case.php
@@ -2,6 +2,7 @@
 
 namespace App\Document;
 
+use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\ODM\MongoDB\Mapping\Annotations as MongoDB;
 
 /**
@@ -25,7 +26,7 @@ class CaseFile
     protected $summary;
 
     /**
-     * @MongoDB\Field(type="date)
+     * @MongoDB\Field(type="date")
      */
     protected $date;
 
@@ -38,6 +39,67 @@ class CaseFile
      * @MongoDB\EmbedMany(targetDocument="Person::class")
      */
     protected $others_involved;
+
+    /**
+     * @MongoDB\Field(type="string")
+     */
+    protected $video;
+
+    public function __construct() {
+        $this->others_involved = new ArrayCollection();
+    }
+
+    public function getId(): ?string {
+        return $this->id;
+    }
+
+    public function getDescription(): ?string {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): void {
+        $this->description = $description;
+    }
+
+    public function getSummary(): ?string {
+        return $this->summary;
+    }
+
+    public function setSummary(string $summary): void {
+        $this->summary = $summary;
+    }
+
+    public function getDate(): ?DateTime {
+        return $this->date;
+    }
+
+    public function setDate(DateTime $date): void {
+        $this->date = $date;
+    }
+
+    public function getPrimaryPerson(): ?Person {
+        return $this->primary_person;
+    }
+
+    public function setPrimaryPerson(Person $PrimaryPerson): void {
+        $this->primary_person = $PrimaryPerson;
+    }
+
+    public function getOthersInvolved(): Collection {
+        return $this->others_involved;
+    }
+
+    public function addOtherInvolved(Person $OtherInvolved): void {
+        $this->others_involved[] = $OtherInvolved;
+    }
+
+    public function getVideo(): ?string {
+        return $this->video;
+    }
+
+    public function setVideo(string $video): void {
+        $this->video = $video;
+    }
 }
 
 /**
@@ -46,7 +108,7 @@ class CaseFile
 class Person
 {
     /**
-     * @MondgoDB\Field(type="string")
+     * @MongoDB\Field(type="string")
      */
     protected $name;
 
@@ -58,5 +120,29 @@ class Person
     /**
      * @MongoDB\Field(type="hash")
      */
-    protected $traits;
+    protected $traits = [];
+
+    public function getName(): ?string {
+        return $this->name;
+    }
+
+    public function setName(string $name): void {
+        $this->name = $name;
+    }
+
+    public function getRole(): ?string {
+        return $this->role;
+    }
+
+    public function setRole(string $role): void {
+        $this->role = $role;
+    }
+
+    public function getTraits(): array {
+        return $this->traits;
+    }
+
+    public function addTrait(string $trait): void {
+        $this->traits[] = $trait;
+    }
 }
